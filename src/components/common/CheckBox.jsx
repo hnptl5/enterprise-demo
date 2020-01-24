20b4cd6300cdcf8f8c7dcb26a982cfced83d3580
Zerell <jackafella2@gmail.com>
2020-01-24 10:25:16 -0700

    updated radio buttons on Onboarding page

--- src/components/common/CheckBox.jsx
+++ src/components/common/CheckBox.jsx
@@ -12,14 +12,20 @@ import CircleUnchecked from '@material-ui/icons/RadioButtonUnchecked';
 
 const checkBoxStyles = theme => ({
     root: {
+      '&:hover':{
+        backgroundColor: 'transparent'
+      },
         color: green[400],
       '&$checked': {
-        color: green[600]
+        color: green[600],
+        backgroundColor: 'transparent'
       },
       margin: '25px',
       transform:' scale(4)',
+      
     },
-    checked: {},
+    checked: {}
+    
    })
 
 const CustomCheckbox = withStyles(checkBoxStyles)(Checkbox);
@@ -39,58 +45,11 @@ export default function CheckboxLabels() {
   return (
     <FormGroup>
     <CustomCheckbox
-    icon={<CircleUnchecked />}
-    checkedIcon={<CircleCheckedFilled />}
+    style= {{width: 36, height: 36}}
+    icon={<CircleUnchecked style={{ fontSize: 20}}/>}
+    checkedIcon={<CircleCheckedFilled style={{ fontSize: 20}}/>}
+    
   />
     </FormGroup>
   );
 }
-
-
-
-
-/**
- * 
- * import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { green } from '@material-ui/core/colors';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-
-const GreenCheckbox = withStyles({
-  root: {
-    color: green[400],
-    '&$checked': {
-      color: green[600]
-    },
-    transform:' scale(4)',
-    borderRadius: '80%'
-  },
-  checked: {}
-})(props => <Checkbox color="default" {...props} />);
-
-export default function CheckboxLabels() {
-  const [state, setState] = React.useState({
-    checkedA: true,
-    checkedB: true,
-    checkedF: true,
-    checkedG: true
-  });
-
-  const handleChange = name => event => {
-    setState({ ...state, [name]: event.target.checked });
-  };
-
-  return (
-    <FormGroup>
-      <GreenCheckbox
-        checked={state.checkedG}
-        onChange={handleChange('checkedG')}
-        value="checkedG"
-      />
-    </FormGroup>
-  );
-}
-
- */
